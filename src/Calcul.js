ed06d8ee6d1e4736603a4ebaae8d04bd9cceac9e
Mael <mael.thomas@data.gouv.fr>
2020-07-08 17:43:06 +0200

    Le calcul fonctionne :)

--- src/Calcul.js
+++ src/Calcul.js
@@ -1,9 +1,12 @@
-import Engine, { formatValue } from "publicodes";
-import rules from "./modèle.yaml";
+import Engine, { formatValue } from 'publicodes'
+import rules from './modèle.yaml'
 
 // On initialise un moteur en lui donnant le publicode.
 // Ce publicode va être parsé
-const engine = new Engine(rules);
-const résultat = engine.evaluate("empreinte");
+const engine = new Engine(rules)
+const résultat = engine.evaluate('empreinte . par km voyageur')
+console.log(résultat)
 
-export default `Empreinte de l'écotrain : ${formatValue(résultat)}`;
+export default `Empreinte de l'écotrain : ${formatValue(résultat, {
+	precision: 1,
+})}`
