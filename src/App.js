14472ebb35832a9aac1511b34fce1c929edf70cf
Mael <mael.thomas@data.gouv.fr>
2020-08-06 11:11:30 +0200

    Première page de doc

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
-import ecotrain from "./ecotrain sur un pont.png";
-import "./App.css";
+import React from 'react'
+import ecotrain from './ecotrain sur un pont.png'
+import './App.css'
 // On définit une liste de règles publicodes
-import résultat from "./Calcul.js";
+import { résultat, engine } from './Calcul.js'
+import { Documentation } from 'publicodes'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ecotrain</h1>
-        <img src={ecotrain} className="App-logo" alt="logo" />
-        <p>{résultat}</p>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<header>
+				<h1>Ecotrain</h1>
+				<img src={ecotrain} title="L'écotrain sur un pont dans la nature" />
+			</header>
+			<Documentation engine={engine} documentationPath={'/documentation'} />
+			{/* Composants de l'app */}
+		</Router>
+	)
 }
 
-export default App;
+export default App
--- src/Calcul.js
+++ src/Calcul.js
@@ -3,10 +3,9 @@ import rules from './modèle.yaml'
 
 // On initialise un moteur en lui donnant le publicode.
 // Ce publicode va être parsé
-const engine = new Engine(rules)
-const résultat = engine.evaluate('empreinte . par km voyageur')
-console.log(résultat)
+export const engine = new Engine(rules)
+const evaluation = engine.evaluate('empreinte . par km voyageur')
 
-export default `Empreinte de l'écotrain : ${formatValue(résultat, {
+export const résultat = `Empreinte de l'écotrain : ${formatValue(evaluation, {
 	precision: 1,
 })}`
