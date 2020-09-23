d9a392845d622c3a28ed0a111fedbb6d070e0b86
Mael <mael.thomas@data.gouv.fr>
2020-09-23 16:00:14 +0200

    Image

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
 			<Router>
 				<header>
 					<h1>Vélo libre</h1>
+					<img css="max-height: 400px" src="https://i.imgur.com/vVlpvHR.jpg" />
 				</header>
 				<div>
 					<p>Un vélo aussi libre qu'un logiciel libre.</p>
