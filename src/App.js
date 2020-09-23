bb46211303be61d0fa03c9a99ef22e7e43ee1c50
Mael <mael.thomas@data.gouv.fr>
2020-09-23 15:57:49 +0200

    Vélo libre

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import ecotrain from './ecotrain sur un pont.png'
 import './App.css'
-// On définit une liste de règles publicodes
-import { résultat, engine } from './Calcul.js'
-import { Documentation } from 'publicodes'
 import {
 	BrowserRouter as Router,
 	Redirect,
@@ -21,39 +17,11 @@ function App() {
 		>
 			<Router>
 				<header>
-					<h1>Écotrain - empreinte carbone</h1>
-					<img
-						src={'/' + ecotrain}
-						css="width: 20rem"
-						title="L'écotrain sur un pont dans la nature"
-					/>
+					<h1>Vélo libre</h1>
 				</header>
 				<div>
-					<p>
-						L'empreinte totale de l'
-						<a href="https://wiki.lafabriquedesmobilites.fr/wiki/ECOTRAIN">
-							écotrain
-						</a>{' '}
-						est estimée à <strong>{résultat}</strong>
-					</p>
-					<p>
-						Cet excellente estimation tranche avec tous les autres modes de
-						transport qui sont largement plus polluants, excepté le TGV qui
-						obtient un résultat similaire.{' '}
-					</p>
-					<p>
-						Nous publions donc ci-dessous le calcul dans son intégralité pour
-						que chacun puisse le comprendre et le remettre en question.
-					</p>
+					<p>Un vélo aussi libre qu'un logiciel libre.</p>
 				</div>
-				<h2>Explication du calcul</h2>
-				<Switch>
-					<Route path="/">
-						<Redirect to="/empreinte/par-km-voyageur" />
-					</Route>
-				</Switch>
-				<Documentation engine={engine} documentationPath={''} />
-				{/* Composants de l'app */}
 			</Router>
 		</div>
 	)
