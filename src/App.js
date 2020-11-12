72bd2a50f528d43d6a943968ff9023d8c63c3230
Mael <mael.thomas@data.gouv.fr>
2020-11-12 19:29:41 +0000

    Nouveau design

--- src/App.js
+++ src/App.js
@@ -12,17 +12,17 @@ function App() {
 		<div
 			css={`
 				margin: 0 auto;
-				max-width: 800px;
 			`}
 		>
 			<Router>
-				<header>
-					<h1>Vélo libre</h1>
-					<img css="max-height: 400px" src="https://i.imgur.com/vVlpvHR.jpg" />
+				<header css="margin-left: 1rem">
+					<h1>vélo<span css="border-radius: .3rem; padding: .1rem .6rem ;margin-left: .2rem; background: black; color: white">libre</span></h1>
 				</header>
-				<div>
-					<p>Un vélo aussi libre qu'un logiciel libre.</p>
+				<div css="display:flex; max-width: 90vw; margin: 0 auto; flex-wrap: wrap; justify-content: center" >
+					<p css="max-width: 50rem;font-size: 400%">Un vélo aussi libre qu'un logiciel libre.</p>
+					<img css="border-radius: .6rem;max-height: 400px" src="https://i.imgur.com/vVlpvHR.jpg" />
 				</div>
+					<p css="text-align: center">Bientôt chez vous.</p>
 			</Router>
 		</div>
 	)
